c3ef68a2e90d5b4125f82d479501821cdfb9e31a
Egor Dronov <dronovlive@gmail.com>
2025-12-09 23:07:07 +0300

    Rename files

--- domium-ui/src/app/(public)/(home)/page.tsx
+++ domium-ui/src/app/(public)/(home)/page.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from 'next'
 
+import { Button } from '@/components/ui/Button'
+
 export const metadata: Metadata = {
   title: 'Главная страница',
   description: 'Описание'
 }
 
 export default function HomePage() {
-  return <div>Hello!</div>
+  return (
+    <div>
+      <Button variant={'default'}>Hello World!</Button>
+    </div>
+  )
 }
